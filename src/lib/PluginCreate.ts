395d8e19171258cf617aa0e903667ce9d2cd9185
maxscharwath <maxscharwath@gmail.com>
2021-03-03 17:47:49 +0100

    [FIX] PluginCreate path error

--- src/lib/PluginCreate.ts
+++ src/lib/PluginCreate.ts
@@ -14,7 +14,7 @@ export default class PluginCreate {
     public async run(noPrompt = false): Promise<void> {
         this.mkdir();
         await download("studimax/streamboard-simple-plugin", this.targetDir);
-        const packageInfo = await fs.readJSON(path.resolve(this.pluginName, "package.json"));
+        const packageInfo = await fs.readJSON(path.resolve(this.targetDir, "package.json"));
         packageInfo.name = this.pluginName;
         const defaultIdentifier = "com.myname." + this.pluginName;
         packageInfo.identifier = noPrompt ? defaultIdentifier :
@@ -25,7 +25,7 @@ export default class PluginCreate {
                 default: () => defaultIdentifier,
                 validate: (input: string) => !!input.match("^[a-z]{2,}\\.[a-z]{2,}\\.[a-z-]{2,}$")
             }])).identifier;
-        await fs.writeJSON(path.resolve(this.pluginName, "package.json"), packageInfo, {spaces: "\t"});
+        await fs.writeJSON(path.resolve(this.targetDir, "package.json"), packageInfo, {spaces: "\t"});
         console.log(green("Installation..."));
         await this.pkgManager.install(this.targetDir);
         console.log(green("Plugin project created successfully"));
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -7,8 +7,9 @@ import fs from "fs-extra";
 describe("StreamBoard cli", () => {
     const pluginName = "plugin";
     const targetDir = path.resolve(__dirname, pluginName);
-    if (fs.existsSync(targetDir)) fs.removeSync(targetDir);
+    console.log(targetDir);
     it("plugin creation", done => {
+        if (fs.existsSync(targetDir)) fs.removeSync(targetDir);
         const creator = new PluginCreate(pluginName, targetDir);
         creator.run(true)
             .then(() => {
